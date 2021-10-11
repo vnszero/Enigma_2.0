383d5adf83e865d8cf6a12e2bd7e9c33dc2baa55
Vinícius N. Silva <48424278+vnszero@users.noreply.github.com>
2021-10-11 18:01:55 -0300

    encrypt device working

--- encrypt_device/main.py
+++ encrypt_device/main.py
@@ -1,4 +1,9 @@
 import random
+
+FLOOR = 32
+ROLLBACK = 90
+CEIL = 122
+
 def word_steam(message : str) -> str:
     #remove acent
     in_table = "áéíóúâêôçãẽõü"
@@ -8,37 +13,52 @@ def word_steam(message : str) -> str:
     fixed_message = no_accents
     return fixed_message
 
+def cesar_shift(message : str) -> list:
+    shift = random.randint(FLOOR,CEIL)
+    cesar_message = ''
+    for alpha in message:
+        code = ord(alpha) + shift
+        if code > CEIL:
+            code -= ROLLBACK
+        cesar_message += chr(code)
+    #return message, shift
+    return cesar_message, shift
+
+def fog_maker(message : str) -> list:
+    fog_num = random.randint(FLOOR,CEIL) # don't use A to avoid weak codes
+    fog_pos = random.randint(FLOOR, fog_num) # don't use A to avoid weak codes
+    fog_message = ''
+    for alpha in message:
+        i = 1
+        while i <= fog_num:
+            if i == fog_pos:
+                fog_message += alpha
+            else:
+                smog = random.randint(FLOOR,CEIL)
+                fog_message += chr(smog)
+            i+=1 
+    #return message, fog_num, fog_pos
+    return fog_message, fog_num, fog_pos
+
 def main():
     message = input('Enter your message: ')
     pre_message = word_steam(message)
     
     # verify empty message
     if pre_message != '':
-        # ascii
-        alpha = list()
-        for i in range(91):
-            alpha.append(i+31)
+        # Cesar Code
+        cesar_message, shift = cesar_shift(pre_message)
 
-        # the set
-        new_message = ''
+        # fog_maker
+        fog_message, fog_num, fog_pos = fog_maker(cesar_message)
 
-        # random dictionary with the new codes
-        random_pos = dict()
-        while len(alpha) != 0:
-            new_pos = random.randint(0,len(alpha)-1)
-            new_message = new_pos+'{'
-            l = len(alpha)
-            random_pos[l] = alpha.pop(new_pos)
+        # zip
+        crypt_message = chr(shift) + chr(fog_num) + chr(fog_pos) + fog_message
 
-        print(random_pos)
-
-        # message content
-        for token in pre_message:
-            code = chr(random_pos[ord(token)-32])
-            new_message += code
-            #print(ord(token)-ord(code))
-
-        #print(new_message)
+        # export
+        print(crypt_message)
+        with open('code.txt', 'w') as file:
+            file.write(crypt_message)
 
 if __name__ == '__main__':
     main()
